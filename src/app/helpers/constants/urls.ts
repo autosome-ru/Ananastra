befd3132c38a802a59a9172993efdd716fc84b66
wish1 <aswq22013@gmail.com>
2020-11-25 02:59:21 +0300

    fixes

--- src/app/helpers/constants/urls.ts
+++ src/app/helpers/constants/urls.ts
@@ -1,2 +1,2 @@
-// export const apiUrl = 'http://ananastra.autosome.ru/api/v2/ananastra';
-export const apiUrl = 'http://localhost:5000/api/v2/ananastra';
+export const apiUrl = 'http://ananastra.autosome.ru/api/v2/ananastra';
+// export const apiUrl = 'http://localhost:5000/api/v2/ananastra';
